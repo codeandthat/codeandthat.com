2462fab127f3a4f9c54cbe85c539536f073852f2
Mat Ryer <thatmatryer@gmail.com>
2015-05-14 23:46:17 +0100

    few tweaks

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(function(){
 	var body = $("body");
 	$(window).click(function(){ body.toggleClass("even"); });
-	$(".email").click(function(){
+	$(".email").click(function(e){
+		e.preventDefault();
 		location.href = "mailto:laurie@codeandthat.com"
 	});
 });
